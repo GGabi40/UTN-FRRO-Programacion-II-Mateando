7892bcdcb0c014d83251b6b36a5edc22baf74abe
ggabi40 <gabibc2000@hotmail.com>
2024-09-07 22:46:51 -0300

    update style and html

--- assets/js/backToTop.js
+++ assets/js/backToTop.js
@@ -29,3 +29,29 @@ function mover(n) {
   const desplazamiento = -index * 100;
   imagenes.style.transform = `translateX(${desplazamiento}%)`;
 }
+
+/* MENU MOBILE */
+
+function menu() {
+  const menuBtn = document.querySelector('.menu-btn');
+  const containMenuMobile = document.querySelector('.mobile');
+  const menuItems = document.querySelectorAll('.menu-list-mob li a');
+
+  menuBtn.addEventListener('change', () => {
+      if (menuBtn.checked) {
+          containMenuMobile.style.display = 'block';
+      } else {
+          containMenuMobile.style.display = 'none';
+      }
+
+  });
+
+  menuItems.forEach(item => {
+      item.addEventListener('click', () => {
+          menuBtn.checked = false;
+          containMenuMobile.style.display = 'none';
+      });
+  });
+};
+
+menu();
