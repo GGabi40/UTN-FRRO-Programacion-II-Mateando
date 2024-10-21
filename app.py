9d2affa2384eddf186fd2500fa43686179f1a531
ggabi40 <gabibc2000@hotmail.com>
2024-10-21 15:51:48 -0300

    rutas a assets

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 # Es el archivo principal donde definirás las rutas, 
 # la conexión a la base de datos y el manejo de las vistas.
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    return render_template('/index.html')
+
+
+if __name__=='__main__':
+    app.run(debug=True)
