55539edc16b6b5917fe0eb3a0fc4032ce44d6937
ggabi40 <gabibc2000@hotmail.com>
2024-10-30 16:59:45 -0300

    jinja added, routes defined

--- app.py
+++ app.py
@@ -1,14 +1,66 @@
-# Es el archivo principal donde definirás las rutas, 
-# la conexión a la base de datos y el manejo de las vistas.
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+import os
 
-app = Flask(__name__)
+# Config
+db_path: str = 'sqlite:///' + os.path.abspath(os.path.dirname(__file__)) + '/db/db.sqlite'
+
+app: Flask = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# -----
+
+# Rutas:
+
 @app.route('/')
-
 def index():
     return render_template('/index.html')
 
+
+@app.route('/contacto')
+def contacto():
+    return render_template('/contacto.html')
+
+
+@app.route('/productos')
+def productos():
+    return render_template('/productos.html')
+
+
+@app.route('/faq')
+def faq():
+    return render_template('/faq.html')
+
+
+@app.route('/mateTips')
+def mateTips():
+    return render_template('/mateTips.html')
+
+@app.route('/quienesSomos')
+def quienesSomos():
+    return render_template('/quienesSomos.html')
+
+
+@app.route('/carrito')
+def carrito():
+    return render_template('/carrito.html')
+
+
+@app.route('/recuperarContrasenia')
+def recuperarContrasenia():
+    return render_template('/recuperarContrasenia.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
+
+@app.route('/registrarse')
+def registrarse():
+    return render_template('/registrarse.html')
+
+# ----
 
 if __name__=='__main__':
     app.run(debug=True)
