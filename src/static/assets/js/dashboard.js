ea3410ad58bd4dc8f8970aecbf0cc36cf69be15b
ggabi40 <gabibc2000@hotmail.com>
2024-11-04 16:48:22 -0300

    agg buscador en dashboard

--- src/static/assets/js/dashboard.js
+++ src/static/assets/js/dashboard.js
@@ -2,17 +2,81 @@ function toggleDarkMode() {
     document.body.classList.toggle('dark-mode');
 }
 
-// document.getElementById('agregar-producto-form').addEventListener('submit', function (e) {
-//     // e.preventDefault();
-    
-//     // Aquí iría la lógica para enviar los datos del formulario al servidor
-//     console.log('Producto agregado:', {
-//         nombre: this.nombre.value,
-//         precio: this.precio.value,
-//         image_url: this.image_url.value,
-//         categoria: this.categoria.value
-//     });
-//     this.reset();
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.getElementById('agregar-producto-form');
 
-// AGREGAR VALIDACION DE FORMULARIO
+    formulario.addEventListener('submit', validaProductos);
+})
+
+const validaProductos = (e) => {
+    e.preventDefault();
+
+    const nombre = document.querySelector('#nombre').value;
+    const categoria = document.querySelector('#categoria').value;
+    const precio = document.querySelector('#precio').value;
+
+    const nombreValidado = verificaNombre(nombre.trim());
+    const categoriaValidado = verificaCategoria(categoria);
+    const precioValidado = verificaPrecio(precio);
+
+    if(nombreValidado && categoriaValidado && precioValidado) {
+        Swal.fire({
+            icon: 'success',
+            title: '¡Listo!',
+            text: 'Se ha agregado el producto con éxito.',
+            confirmButtonText: 'Aceptar'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                e.target.submit();
+            }
+        });
+    }
+}
+
+const verificaPrecio = (precio) => {
+    if (precio <= 0) {
+        mensajeError('Precio', 'El precio debe ser mayor que 0.');
+        return false;
+    }
+
+    if (isNaN(precio)) {
+        mensajeError('Precio', 'El precio debe ser un número.');
+        return false;
+    }
+
+    ocultarError('Precio');
+    return true;
+}
+
+const verificaCategoria = (categoria) => {
+    if (categoria == '') {
+        mensajeError('Categoria', 'Debe elegir una categoría.');
+        return false;
+    }
+
+    ocultarError('Categoria');
+    return true;
+}
+
+const verificaNombre = (nombre) => {
+    if (nombre == '' || nombre == ' ' || nombre.length < 3) {
+        mensajeError('Nombre', 'El nombre debe tener al menos 3 caracteres.');
+        return false;
+    }
+
+    ocultarError('Nombre')
+    return true;
+}
+
+// Mensaje:
+function mensajeError(id, msj) {
+    const elemento = document.getElementById(`error${id}`);
+
+    elemento.style.color = 'red';
+    elemento.textContent = msj;
+}
+
+function ocultarError(id) {
+    const elemento = document.getElementById(`error${id}`);
+    elemento.textContent = '';
+}
