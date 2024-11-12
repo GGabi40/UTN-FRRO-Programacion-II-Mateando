79f86e5290c21417df00d29fb6ebc4fbf6bd14fe
ggabi40 <gabibc2000@hotmail.com>
2024-11-12 16:19:49 -0300

    deploy

--- src/config.py
+++ src/config.py
@@ -1,13 +1,5 @@
-""" import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
- """
-
 class Config():
     SECRET_KEY='1234'
-
+    
     SQLALCHEMY_DATABASE_URI = 'sqlite:///mateando.db'
     SQLALCHEMY_TRACK_MODIFITIATIONS = False
