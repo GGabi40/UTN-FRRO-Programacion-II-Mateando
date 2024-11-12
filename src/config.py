7982b5ba479dca379d4de554aaee4c2fe40583d6
ggabi40 <gabibc2000@hotmail.com>
2024-11-12 14:41:34 -0300

    cambios en config para deploy

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
-class Config():
-    SECRET_KEY='1234'
-    
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mateando.db'
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "1234")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///mateando.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
