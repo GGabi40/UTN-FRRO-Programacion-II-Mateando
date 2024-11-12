417ee9a7baf7af854a7cd385c38c083e401c4070
Gabriela B Carvalho <107872122+GGabi40@users.noreply.github.com>
2024-11-12 15:52:19 -0300

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
-class Config():
-    SECRET_KEY='1234'
-    
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///mateando.db'
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY')
